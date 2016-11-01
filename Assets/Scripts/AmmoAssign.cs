0b5b99712376288151835d946ab4d0c7303c3dda
Spencer Obsitnik <obss@domain1.com>
2016-11-01 16:13:21 -0400

    ammo vending machine prefab, more weapons stuff, screw the weapons stuff

---
+++ Assets/Scripts/AmmoAssign.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AmmoAssign : MonoBehaviour {
+
+    private VRButtonExample button;
+    public Transform spawnPoint;
+    public GameObject magazinePrefab;
+
+    public enum ammoType
+    {
+        revolver,
+        shotgun,
+        smg,
+        assault,
+        sniper,
+        sniperCal,
+        grenadeLauncher,
+        minigun,
+        none
+    }
+    private string num;
+    public ammoType typeOfAmmo = ammoType.none;
+
+    // Use this for initialization
+    void Start() {
+        button = GetComponent<VRButtonExample>();
+        switch (typeOfAmmo)
+        {
+            case ammoType.revolver:
+                num = "1";
+                break;
+            case ammoType.shotgun:
+                num = "2";
+                break;
+            case ammoType.smg:
+                num = "2";
+                break;
+            case ammoType.assault:
+                num = "2";
+                break;
+            case ammoType.sniper:
+                num = "2";
+                break;
+            case ammoType.sniperCal:
+                num = "2";
+                break;
+            case ammoType.grenadeLauncher:
+                num = "2";
+                break;
+            case ammoType.minigun:
+                num = "2";
+                break;
+            default:
+                num = "0";
+                break;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/AmmoSpitter.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AmmoSpitter : MonoBehaviour
+{
+
+    public GameObject magazinePrefab;
+    public Vector3 spawnPoint;
+
+    public void Activate()
+    {
+        
+        //GameObject magazinePrefab = (GameObject)Instantiate(magazinePrefab, spawnPoint, Quaternion.identity);
+    }
+}
