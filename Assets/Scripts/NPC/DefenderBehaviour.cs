6970263e19a6be728e8ab31dbddda5b53827cbd1
gtspencer <spencerobsitnik@gmail.com>
2016-11-14 16:43:24 -0500

    some stuff i did not sure

--- Assets/Scripts/NPC/DefenderBehaviour.cs
+++ Assets/Scripts/NPC/DefenderBehaviour.cs
@@ -3,7 +3,26 @@ using System.Collections;
 
 public class DefenderBehaviour : MonoBehaviour
 {
+    //path around bunker to defend
+    public Transform pointA;
+    public Transform pointB;
+    public Transform pointC;
+    public Transform pointD;
 
+    private GameObject target;
+    private Vector3 targetPos;
+
+
+    public GameObject collisionObject;
+    public bool attackMode;
+
+
+    public float speed = 0.1f;
+    public int hp = 100;
+    public float attackCooldown = 1.0f;
+    public int attackPoints = 1;
+    public float killThreshhold = 1.5f;
+    public Color defaultColor;
     // Use this for initialization
     void Start()
     {
@@ -14,5 +33,10 @@ public class DefenderBehaviour : MonoBehaviour
     void Update()
     {
 
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+    
     }
 }
--- Assets/Scripts/Weapons and Ammo/ItsATrap.cs
+++ Assets/Scripts/Weapons and Ammo/ItsATrap.cs
@@ -5,27 +5,40 @@ public class ItsATrap : MonoBehaviour
 {
     public int hp;
     public int attackPoints;
+    public float attackCooldown;
     private EnemyBehaviour enemyBehaviour;
+    private bool trapping;
 
     // Use this for initialization
     void Start()
     {
-        
+        trapping = false;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-
+        if (trapping)
+        {
+            StartCoroutine("DealDamage");
+        }
     }
 
-    void OnCollisionTrigger(Collision collision)
+    void OnCollisionEnter(Collision collision)
     {
         GameObject collisionObject = collision.gameObject;
-        if (collisionObject.name == "Enemy")
+        if (collisionObject.tag == "Enemy")
         {
             enemyBehaviour = collisionObject.GetComponent<EnemyBehaviour>();
-            //enemyBehaviour.processDamage(attackPoints);
+            trapping = true;
         }
+    }
+
+    public IEnumerator DealDamage()
+    {
+        trapping = false;
+        enemyBehaviour.ProcessDamage(attackPoints);
+        yield return new WaitForSeconds(trapCooldown);
+        trapping = true;
     }
 }
